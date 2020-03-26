7841996facf2be2135cf65e24e0f1890cfa91fe9
unknown <ahabey7@gmail.com>
2020-03-26 11:53:29 +0100

    categories done

--- models/fitness.js
+++ models/fitness.js
@@ -14,6 +14,10 @@ const fitnessSchema = new Schema({
         type:Date,
         default:Date.now
       },
+      category:{
+        type:String,
+        enum:["Equipment", "Apparel", "Accessories"]
+      },
       price:{
           type:Number
       },
